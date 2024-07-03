c836e9bc1c5c30d9aeb18db60946b477307496b8
Ryan Richards <rrichards032@gmail.com>
2024-07-03 10:09:05 -0400

    pivot to javascript API for discord

---
+++ src/discord-bot/index.js
@@ -0,0 +1,37 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config()
+const client = new Client({ intents: [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent
+] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+
+  if (interaction.commandName === 'lootcount') {
+    await interaction.reply("You'll own nothing and you'll be happy ✨");
+  }
+});
+
+client.on('messageCreate', (msg) => {
+  if (msg.author.bot){
+    return;
+  }
+  console.log(msg.content)
+  if (msg.content === 'yello') {
+    msg.reply("I'm glad Longfellow is on a dirge. The scene is safe.")
+  }
+
+
+  
+})
+client.login(process.env.DISCORD_TOKEN);
